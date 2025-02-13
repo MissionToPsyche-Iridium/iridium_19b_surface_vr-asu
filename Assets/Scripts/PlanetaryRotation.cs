f935f4da6c22d38e09ace394bd11efd8ea03eb8b
Andrew-Tan-622 <95309123+Andrew-Tan-2025@users.noreply.github.com>
2025-02-13 12:57:56 -0700

    1st commit
    Rotating Asteroid

---
+++ Assets/Scripts/PlanetaryRotation.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlanetaryRotation : MonoBehaviour
+{
+    [SerializeField] Vector3 axialTilt;
+    [SerializeField] float _period; //time taken for 1 rotation
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(axialTilt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set axial tilt
+        CelestialRotation();
+    }
+
+    void CelestialRotation()
+    {
+        float radius = GetComponent<SphereCollider>().radius; //will change. Not Robust enough
+
+        float rotation = (2 * Mathf.PI * radius) / _period;
+        Vector3 rotate = new Vector3(0, rotation, 0);
+        transform.Rotate(rotate * Time.deltaTime);
+    }
+}
