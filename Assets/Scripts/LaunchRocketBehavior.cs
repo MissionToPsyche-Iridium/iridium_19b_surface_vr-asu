a54ee75130dcd725c810cf826ec7cb9f88ed6730
cqlia <73913463+cqlia@users.noreply.github.com>
2025-04-10 13:47:25 -0700

    remove video player

--- Assets/Scripts/LaunchRocketBehavior.cs
+++ Assets/Scripts/LaunchRocketBehavior.cs
@@ -2,14 +2,31 @@ using UnityEngine;
 
 public class LaunchRocketBehavior : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float _waitTime = 0.0f;
+    private bool _inLaunch = false;
+
+    private float _acceleration = 0.0f;
+    private float _velocity = 0.0f;
+
     void Start()
     {
+        RenderSettings.skybox.SetFloat("_Blend", 0.0f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (_inLaunch) {
+            _velocity += _acceleration * Time.deltaTime;
+            transform.position += new Vector3(0, _velocity * Time.deltaTime, 0);
+            var blend = Mathf.Min(transform.position.y / 40.0f, 1.0f);
+            RenderSettings.skybox.SetFloat("_Blend", blend);
+        } else {
+            _waitTime += Time.deltaTime;
+        }
+
+        if (!_inLaunch && _waitTime > 16.0f) {
+            _inLaunch = true;
+            _acceleration = 5.0f;
+        }
     }
 }
